a6c6b312e6e89a5c12e320c0114acd1aae4e59c3
Dustin Micklos <damicklo5@gmail.com>
2021-04-14 14:57:10 -0500

    connecting to server, but getting 404 when trying to create user

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
